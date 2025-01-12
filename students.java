cb3410191b0d7d4e360af145c95c74f40ad1ffd8
pristina singh <singhpristina@892gmail.com>
2025-01-12 13:04:43 -0800

    changed

---
+++ students.java
@@ -0,0 +1,28 @@
+package week8;
+
+public class students {
+	// Student.java
+	public class Student {
+	    private String name;
+	    private int age;
+
+	    public Student(String name, int age) {
+	        this.name = name;
+	        this.age = age;
+	    }
+
+	    // Getter methods
+	    public String getName() {
+	        return name;
+	    }
+
+	    public int getAge() {
+	        return age;
+	    }
+	}
+
+}
+	
+	    
+
+
